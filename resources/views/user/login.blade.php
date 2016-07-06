e9b681d4688329199e2d227396c5d08b2e72a51a
gurgenmosesyan <gurgen.mosesyan@gmail.com>
2016-07-07 00:42:51 +0400

    commit

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,31 +1,55 @@
 <?php
 use App\Models\User\User;
 $head->appendScript('/js/user.js');
-?>
 
+$title = trans('www.login.title');
+
+?>
 @extends('layout')
 
 @section('content')
 
-    <form id="login-form" action="{{url_with_lng('/api/login', false)}}" method="post">
-        <div>
-            <input type="text" name="email" /> Email
-            <div id="form-error-email" class="form-error"></div>
+<div class="page">
+    <div id="top-banner" class="tc">
+        <a href="#">
+            <img src="/images/temp/top-banner.jpg" />
+        </a>
+    </div>
+    <div id="login-block">
+        <div id="login-inner">
+            <h1 class="tc">{{$title}}</h1>
+            <div class="fb-login fl">
+                <a href="#" id="fb-login" class="db"></a>
+            </div>
+            <div class="google-login fl">
+                <a href="#" id="google-login" class="db"></a>
+            </div>
+            <div class="cb"></div>
+            <div class="line-block">
+                <div class="line fl"></div>
+                <div class="or fl tc">{{trans('www.base.label.or')}}</div>
+                <div class="line fl"></div>
+                <div class="cb"></div>
+            </div>
+            <form id="login-form" action="{{url_with_lng('/api/login', false)}}" method="post">
+                <div class="form-box">
+                    <input type="text" name="email" placeholder="{{trans('www.base.label.email')}}" />
+                    <div id="form-error-email" class="form-error"></div>
+                </div>
+                <div  class="form-box">
+                    <input type="password" name="password" placeholder="{{trans('www.base.label.password')}}" />
+                    <div id="form-error-password" class="form-error"></div>
+                </div>
+                <?php /* <div>
+                    <input type="checkbox" name="remember_me" value="{{User::REMEMBER_ME}}" /> Remember me
+                    <div id="form-error-remember_me" class="form-error"></div>
+                </div> */ ?>
+                {{csrf_field()}}
+                <input type="submit" class="orange-bg fb" value="{{$title}}" />
+            </form>
+            <p class="link tc">{{trans('www.login.link_title')}} <a href="{{url_with_lng('/registration', false)}}" class="orange">{{trans('www.reg.title')}}</a></p>
         </div>
-        <div>
-            <input type="password" name="password" /> Password
-            <div id="form-error-password" class="form-error"></div>
-        </div>
-        <div>
-            <input type="checkbox" name="remember_me" value="{{User::REMEMBER_ME}}" /> Remember me
-            <div id="form-error-remember_me" class="form-error"></div>
-        </div>
-        {{csrf_field()}}
-        <input type="submit" />
-    </form>
-
-    <style>
-        div { margin-bottom: 10px; }
-    </style>
+    </div>
+</div>
 
 @stop
--- resources/views/user/registration.blade.php
+++ resources/views/user/registration.blade.php
@@ -1,49 +1,62 @@
 <?php
 $head->appendScript('/js/user.js');
-?>
 
+$title = trans('www.reg.title');
+
+?>
 @extends('layout')
 
 @section('content')
 
-<form id="registration-form" action="{{url_with_lng('/api/registration', false)}}" method="post">
-    <div>
-        <input type="text" name="email" /> Email
-        <div id="form-error-email" class="form-error"></div>
+<div class="page">
+    <div id="top-banner" class="tc">
+        <a href="#">
+            <img src="/images/temp/top-banner.jpg" />
+        </a>
     </div>
-    <div>
-        <input type="password" name="password" /> Password
-        <div id="form-error-password" class="form-error"></div>
-    </div>
-    <div>
-        <input type="password" name="re_password" /> Re password
-        <div id="form-error-re_password" class="form-error"></div>
-    </div>
-    <div>
-        <input type="text" name="first_name" /> First name
-        <div id="form-error-first_name" class="form-error"></div>
-    </div>
-    <div>
-        <input type="text" name="last_name" /> Last name
-        <div id="form-error-last_name" class="form-error"></div>
-    </div>
-    <div>
-        <input type="text" name="phone" /> Phone
-        <div id="form-error-phone" class="form-error"></div>
-    </div>
-    {{csrf_field()}}
-    <input type="submit" />
-</form>
 
-<br><br><br>
+    <div id="login-block">
+        <div id="login-inner">
+            <h1 class="tc">{{$title}}</h1>
+            <div class="fb-login fl">
+                <a href="#" id="fb-login" class="db"></a>
+            </div>
+            <div class="google-login fl">
+                <a href="#" id="google-login" class="db"></a>
+            </div>
+            <div class="cb"></div>
+            <form id="registration-form" action="{{url_with_lng('/api/registration', false)}}" method="post">
+                <div class="form-box">
+                    <input type="text" name="email" placeholder="{{trans('www.base.label.email')}}" />
+                    <div id="form-error-email" class="form-error"></div>
+                </div>
+                <div class="form-box">
+                    <input type="password" name="password" placeholder="{{trans('www.base.label.password')}}" />
+                    <div id="form-error-password" class="form-error"></div>
+                </div>
+                <div class="form-box">
+                    <input type="password" name="re_password" placeholder="{{trans('www.base.label.re_password')}}" />
+                    <div id="form-error-re_password" class="form-error"></div>
+                </div>
+                <div class="form-box">
+                    <input type="text" name="first_name" placeholder="{{trans('www.base.label.first_name')}}" />
+                    <div id="form-error-first_name" class="form-error"></div>
+                </div>
+                <div class="form-box">
+                    <input type="text" name="last_name" placeholder="{{trans('www.base.label.last_name')}}" />
+                    <div id="form-error-last_name" class="form-error"></div>
+                </div>
+                <div class="form-box">
+                    <input type="text" name="phone" placeholder="{{trans('www.base.label.phone')}}" />
+                    <div id="form-error-phone" class="form-error"></div>
+                </div>
+                {{csrf_field()}}
+                <input type="submit" class="orange-bg fb" value="{{$title}}" />
+            </form>
+            <p class="link tc">{{trans('www.reg.link_title')}} <a href="{{url_with_lng('/login', false)}}" class="orange">{{trans('www.login.title')}}</a></p>
+        </div>
+    </div>
 
-<a href="#" id="fb-login">FB login</a>
-<br>
-<br>
-<a href="#" id="google-login">Google login</a>
-
-<style>
-    div { margin-bottom: 10px; }
-</style>
+</div>
 
 @stop
