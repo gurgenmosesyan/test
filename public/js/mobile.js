9dd9fd1a6df8233143d7a2c5c953f6c17a00f6f4
gurgenmosesyan <gurgen.mosesyan@gmail.com>
2016-11-12 15:13:51 +0400

    commit

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,28 +1,5 @@
 'use strict';
 
-/*$.cookie = function(name, value, options) {
-    options = options || {};
-    if (value === null) {
-        value = '';
-        options.expires = -1;
-    }
-    var expires = '';
-    if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
-        var date;
-        if (typeof options.expires == 'number') {
-            date = new Date();
-            date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
-        } else {
-            date = options.expires;
-        }
-        expires = '; expires=' + date.toUTCString();
-    }
-    var path = options.path ? '; path=' + (options.path) : '';
-    var domain = options.domain ? '; domain=' + (options.domain) : '';
-    var secure = options.secure ? '; secure' : '';
-    document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
-};*/
-
 var $mobile = {};
 
 $mobile.initBurger = function() {
